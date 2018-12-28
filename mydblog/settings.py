94ac834f9e6128a5e7ba3d23d62e098c44da7d53
anishjain <pyanishjain@gmail.com>
2018-12-28 11:08:19 +0000

    p1 deleted and model is added

--- mydblog/settings.py
+++ mydblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3+ynlzdft!6ireq1or29n8mzeam47m6@52t0!d7r@z9g&!mq7s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
